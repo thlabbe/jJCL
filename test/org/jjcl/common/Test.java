f765a3f1f3fabe61f4631560d077d9df4561f7b9
thlabbe <labbe.thomas@gmail.com>
2013-09-13 15:14:09 +0200

    tests

---
+++ test/org/jjcl/common/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.jjcl.common;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author MS
+ *
+ */
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
