6acfd810289c8790b590b5b5887d31ad85ea9c5b
thlabbe <labbe.thomas@gmail.com>
2013-09-13 13:59:50 +0200

    init

---
+++ src/org/jjcl/common/Main.java
@@ -0,0 +1,16 @@
+package org.jjcl.common;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.run();
+
+	}
+
+	private void run() {
+		System.out.println("run");
+		
+	}
+
+}
